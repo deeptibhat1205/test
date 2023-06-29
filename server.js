af9ec711f782ffd84e0baf26d007086da1a994db
cipher <ansh.chauhan269@gmail.com>
2023-06-30 01:22:18 +0530

    the information stres now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
@@ -13,38 +14,20 @@ app.use(bodyParser.json())
 mongoose.connect("mongodb+srv://cipher:5223@cluster0.1kv1ue0.mongodb.net/medbase");
 
 var db = mongoose.connection;
+
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
-//schema
-
-//  const patientSchema = {
-//     patientID: String,
-//     patientName: String,
-//     report: String,
-//     reportAnalysis: String,
-//     reportType: String,
-//     img: String,
-//     uploadAnalysis: String
-//  }
-
 app.post("/", function(req, res){
-    var patientID = req.body.pid;
-    var patientName = req.body.pname;
-    var report = req.body.rep;
-    var reportAnalysis = req.body.rint;
-    var reportType = req.body.upselect;
-    var img = req.body.upfile;
-    var uploadAnalysis = req.body.uint;
-
-    var data = {
-        "patientID": patientID,
-        "patientName": patientName,
-        "report": report,
-        "reportAnalysis": reportAnalysis,
-        "reportType": reportType,
-        "img": img,
-        "uploadAnalysis": uploadAnalysis
+    
+    const data = {
+        patientID: req.body.pid,
+        patientName: req.body.pname,
+        report: req.body.rep,
+        reportAnalysis: req.body.rint,
+        reportType: req.body.upselect,
+        img: req.body.upfile,
+        uploadAnalysis: req.body.uint
     }
 
     db.collection('patient').insertOne(data, (err,collection)=>{
@@ -53,10 +36,9 @@ app.post("/", function(req, res){
         }
         console.log("record inserted");
     });
-
+    return res.redirect('/')
 })
 
-// const Patient = mongoose.model("patient", patientSchema);
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/public/index.html");
@@ -69,22 +51,6 @@ app.get("/", function(req, res){
     })
     return res.redirect("/");
 })
-
-// app.post("/", function(req, res){
-//      let newPatient = new Patient({
-//         patientID: req.body.pid,
-//         patientName: req.body.pname,
-//         report: req.body.rep,
-//         reportAnalysis: req.body.rint,
-//         reportType: req.body.upselect,
-//         img: req.body.upfile,
-//         uploadAnalysis: req.body.uint
-//      });
-
-
-//     newPatient.save();
-//     res.redirect('/');
-// })
 
 
 app.listen(3000, function(){
