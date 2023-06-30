fb6966d5a105664d90d8306f2bbf09f43098fc33
cipher <ansh.chauhan269@gmail.com>
2023-06-30 15:37:33 +0530

    it works

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const bcrypt = require("bcrypt")
 
 const app = express();
 
 var path = require('path')
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public','login')))
+app.use(express.static(path.join(__dirname,'public','doctor')))
 app.use(bodyParser.json())
 
 mongoose.connect("mongodb+srv://cipher:5223@cluster0.1kv1ue0.mongodb.net/medbase");
@@ -18,8 +19,36 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
-app.post("/", function(req, res){
+
+// Patient login forwarding
+// app.post("/", function(req, res){
     
+   
+// })
+
+
+
+// Doctors login forwarding
+app.post("/", function(req, res){
+
+    const hashedDoctorPwd = bcrypt.hash(req.body.dpwd, 10)
+
+    const doctor_login_data = {
+        doctorID: req.body.did,
+        doctorPWD: hashedDoctorPwd
+    }
+    
+    db.collection('doctors').insertOne(doctor_login_data, (err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("record inserted");
+    });
+    return res.sendFile(path.join(__dirname,'public','doctor','doctor.html'))
+})
+
+app.post("/report-submit", function(req, res){
+
     const data = {
         patientID: req.body.pid,
         patientName: req.body.pname,
@@ -30,7 +59,7 @@ app.post("/", function(req, res){
         uploadAnalysis: req.body.uint
     }
 
-    db.collection('patient').insertOne(data, (err,collection)=>{
+    db.collection('report').insertOne(data, (err,collection)=>{
         if(err){
             throw err;
         }
@@ -41,10 +70,12 @@ app.post("/", function(req, res){
 
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"/public/index.html");
+    res.sendFile(path.join(__dirname,'public','login','login.html'));
 })
 
 
+
+// connectin the server
 app.get("/", function(req, res){
     res.set({
             "Allow-access-Allow-Origin": '*'
